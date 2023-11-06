b39db6b31cab453e1474c8984826f03a844f27bb
Karina <karina.steinberg@voco.ee>
2023-11-06 18:26:28 +0200

    navigator setup

--- mobiilirakendus/App.js
+++ mobiilirakendus/App.js
@@ -1,21 +1,24 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView} from 'react-native';
 import Splash from './src/screens/auth/Splash';
 import AuthHeader from './src/components/AuthHeader';
 import Signup from './src/screens/auth/Signup';
 import Input from './src/components/Input';
 import Signin from './src/screens/auth/Signin';
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 //const REVERSED_CLIENT_ID = 'com.googleusercontent.apps.79243600821-jdm74p3nr5frlui1p9hqismip60vui8m'
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
   //useEffect(() => {}, [])
   return (
     <NavigationContainer>
-    <SafeAreaView>
-       <Signin/>
-    </SafeAreaView>
+      <Stack.Navigator>
+          <Stack.Screen name="Splash" component={Splash} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
