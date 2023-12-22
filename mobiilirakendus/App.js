4cf448442ef24bd0372520578ca1e36aee0be08b
Karina <karina.steinberg@voco.ee>
2023-12-22 09:33:27 +0200

    Create navigation between screens and tabs

--- mobiilirakendus/App.js
+++ mobiilirakendus/App.js
@@ -5,32 +5,95 @@ import AuthHeader from './src/components/AuthHeader';
 import Signup from './src/screens/auth/Signup';
 import Input from './src/components/Input';
 import Signin from './src/screens/auth/Signin';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { colors } from './src/utils/colors';
+import {colors} from './src/utils/colors';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-
-//const REVERSED_CLIENT_ID = 'com.googleusercontent.apps.79243600821-jdm74p3nr5frlui1p9hqismip60vui8m'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Home from './src/screens/app/Home';
+import Favorites from './src/screens/app/Favorites';
+import { Image } from "react-native";
+import Profile from './src/screens/app/Profile';
 
 const Stack = createNativeStackNavigator();
 
-const theme = {
-  colors: {
-    background: colors.white
-  },
-};
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+  return (
+    <Tab.Navigator screenOptions={({route}) =>({
+      tabBarIcon: ({ focused, color, size}) => {
+        let icon;
+
+        if (route.name === 'Home') {
+          icon = focused
+          ? require('./src/assets/tabs/clarity_home_solid.png')
+          : require('./src/assets/tabs/clarity_home_grey.png');
+        } else if (route.name === 'Favorites') {
+          icon = focused
+          ? require('./src/assets/tabs/marker_solid.png')
+          : require('./src/assets/tabs/marker_grey.png');
+        } else if (route.name === 'Profile') {
+          icon = focused
+          ? require('./src/assets/tabs/group_solid.png')
+          :require('./src/assets/tabs/group_grey.png');
+        }
+        return <Image style={{width:24, height:24}} source={icon} />
+      },
+      headerShown: false,
+      tabBarShowLabel: false,
+      tabBarStyle: {borderTopColor: colors.lightGrey}
+    })}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Favorites" component={Favorites} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator> 
+  )
+}
 
 const App = () => {
-  //useEffect(() => {}, [])
+  const isSignedIn = true
+
+  const theme = {
+    colors: {
+      background: colors.white
+    },
+  };
   return (
     <SafeAreaProvider>
-    <NavigationContainer theme={theme}>
-      <Stack.Navigator>
-          <Stack.Screen name="Splash" component={Splash} options={{headerShown: false}}/>
-          <Stack.Screen name="Signup" component={Signup} options={{headerShown: false}}/>
-          <Stack.Screen name="Signin" component={Signin} options={{headerShown: false}}/>
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer theme={theme}>
+        <Stack.Navigator>
+          {isSignedIn ? (
+            <>
+              <Stack.Screen
+                name="Tabs"
+                component={Tabs}
+                options={{headerShown: false}}
+              />
+            </>
+          ) : (
+            <>
+              <Stack.Screen
+                name="Splash"
+                component={Splash}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="Signup"
+                component={Signup}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="Signin"
+                component={Signin}
+                options={{headerShown: false}}
+              />
+            </>
+          )}
+        </Stack.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 };
