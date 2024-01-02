bc1c7e0456f65383feed049968693a8a14aa88da
Karina <karina.steinberg@voco.ee>
2024-01-02 17:39:34 +0200

    create homepage screen

--- mobiilirakendus/src/screens/app/Home/index.js
+++ mobiilirakendus/src/screens/app/Home/index.js
@@ -1,30 +1,71 @@
-import React from "react";
-import { View, Text, FlatList } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import {styles} from "./styles";
-import Header from "../../../components/Header";
-import { categories } from "../../../data/categories";
-import CategoryBox from "../../../components/CategoryBox";
-
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {styles} from './styles';
+import Header from '../../../components/Header';
+import {categories} from '../../../data/categories';
+import CategoryBox from '../../../components/CategoryBox';
+import {products} from '../../../data/products';
+import ProductHomeItem from '../../../components/ProductHomeItem';
 
 const Home = () => {
-    const renderCategoryItem = ({item}) => {
-        console.log('item => ', item)
-        return (
-            <CategoryBox title={item?.title} image={item?.image}/>
-        )
+  const [selectedCategory, setSelectedCategory] = useState();
+  const [keyword, setKeyword] = useState();
+  const [selectedProducts, setSelectedProducts] = useState(products);
+
+  useEffect(() => {
+    if(selectedCategory && !keyword) {
+        const updatedSelectedProducts = products.filter((product) => product?.category === selectedCategory)
+        setSelectedProducts(updatedSelectedProducts)
+    } else if(selectedCategory && keyword){
+        const updatedSelectedProducts = products.filter((product) => product?.category === selectedCategory && product?.title?.toLowerCase().includes(keyword.toLowerCase()))
+        setSelectedProducts(updatedSelectedProducts)
+    } else if (!selectedCategory && keyword) {
+        const updatedSelectedProducts = products.filter((product) => product?.title?.toLowerCase().includes(keyword?.toLowerCase()))
+        setSelectedProducts(updatedSelectedProducts)
+    } else if (!keyword && !selectedCategory){
+        setSelectedProducts(products)
     }
+}, [selectedCategory, keyword])
 
-    return(
-        <SafeAreaView>
-            <View style={styles.container}>
-                <Header showSearch={true} title="Find All You Need"/>
-                <FlatList showsHorizontalScrollIndicator={false} style={styles.list} horizontal data={categories} renderItem={renderCategoryItem} keyExtractor={(item, index) =>
-                String(index)} />
-                <Text>Home</Text>
-            </View>
-        </SafeAreaView>
-    )
-}
+  const renderCategoryItem = ({item}) => {
+    return (
+      <CategoryBox
+        onPress={() => setSelectedCategory(item?.id)}
+        isSelected={item.id === selectedCategory}
+        title={item?.title}
+        image={item?.image}
+      />
+    );
+  };
 
-export default React.memo(Home)
+  const renderProductItem = ({item}) => {
+    console.log('item => ', item);
+    return <ProductHomeItem {...item} />;
+  };
+
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Header showSearch={true} onSearchKeyword={setKeyword} keyword={keyword} title="Find All You Need" />
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          style={styles.list}
+          horizontal
+          data={categories}
+          renderItem={renderCategoryItem}
+          keyExtractor={(item, index) => String(index)}
+        />
+        <FlatList
+          numColumns={2}
+          data={selectedProducts}
+          renderItem={renderProductItem}
+          keyExtractor={item => String(item.id)}
+          ListFooterComponent={<View style={{height: 250}} />}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default React.memo(Home);
