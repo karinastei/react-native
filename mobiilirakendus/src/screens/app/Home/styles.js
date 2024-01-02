dc9f009178e98b347b9c941802358f745913a02e
Karina <karina.steinberg@voco.ee>
2024-01-02 13:43:49 +0200

    categories

--- mobiilirakendus/src/screens/app/Home/styles.js
+++ mobiilirakendus/src/screens/app/Home/styles.js
@@ -3,7 +3,10 @@ import { colors } from "../../../utils/colors"
 
 export const styles = StyleSheet.create({
     container: {
-
+        padding: 24,
     },
+    list: {
+        paddingVertical: 24
+    }
     }
 )
--- mobiilirakendus/src/screens/app/Profile/styles.js
+++ mobiilirakendus/src/screens/app/Profile/styles.js
@@ -3,7 +3,7 @@ import { colors } from "../../../utils/colors"
 
 export const styles = StyleSheet.create({
     container: {
-
+        padding: 14,
     },
     }
 )
